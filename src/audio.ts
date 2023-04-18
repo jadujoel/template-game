76205ec44a79ef89722712f752c826b1c749f9d3
Joel Lof <jlof@evolution.com>
2023-04-18 15:49:45 +0200

    feat: initial setup

--- src/audio.ts
+++ src/audio.ts
@@ -2,6 +2,8 @@ import { EcasProviderSingleton as audio } from '@netent-tech/ecas-provider';
 import { type IEcasOptions } from '@netent-tech/ecas-engine'
 (async (): Promise<void> => {
   const searchParams = new URLSearchParams(window.location.search)
+  // make sure we can host the sounds locally without needing to run push and wait for github action
+  // by editing the url to include the ecas-load-path query param
   const loadPath = searchParams.get('ecas-load-path') ?? 'https://jadujoel.github.io/template-sounds/'
   const response = await fetch(`${loadPath}/config.json`)
   const ecasOptions: IEcasOptions = await response.json()
